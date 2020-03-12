ed541dec4ef56142ce99e07edb3ad2bb1c920176
Kevan Zameni <zamenikevan@yahoo.com>
2020-03-11 17:11:18 -0700

    added BuildControls components for UI

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
   // Object.keys returns the Object's keys that you pass into it as an array
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
