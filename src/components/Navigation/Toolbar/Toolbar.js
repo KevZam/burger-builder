dcbfe87424dc14ed6f09c06c90858ca941488c9c
Kevan Zameni <zamenikevan@yahoo.com>
2020-03-12 14:56:10 -0700

    finished with hamburger icon and menu button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,17 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import MenuButton from "../MenuButton/MenuButton";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo></Logo>
-    <NavigationItems />
+    <MenuButton menuOpen={props.openMenu} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
