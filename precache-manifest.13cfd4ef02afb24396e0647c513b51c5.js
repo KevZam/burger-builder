8088b12ede3e6271b39e56035b40a76c24510d06
Kevan Zameni <zamenikevan@yahoo.com>
2020-03-13 10:01:03 -0700

    Updates

---
+++ precache-manifest.13cfd4ef02afb24396e0647c513b51c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e628e7b925b988dc065f1271ecbdc3a",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "fdb194c811e3e647dc7f",
+    "url": "/burger-builder/static/css/main.f9e9bfcb.chunk.css"
+  },
+  {
+    "revision": "432d30993cb91f933691",
+    "url": "/burger-builder/static/js/2.c8229ecb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/burger-builder/static/js/2.c8229ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb194c811e3e647dc7f",
+    "url": "/burger-builder/static/js/main.0a6b02f9.chunk.js"
+  },
+  {
+    "revision": "c1102db0eb5e1e1da252",
+    "url": "/burger-builder/static/js/runtime-main.a5effd22.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/BurgerLogo.b8503d26.png"
+  }
+]);
